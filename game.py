ac94d3413cdf9529b0f57299247d79cb58789a2f
Nicholas <nich@hawaii.edu>
2025-11-08 01:52:42 -1000

    Add pages and FPS uncapping + test upgrades
    added a page feature, more buttons, and improved game logic slightly

--- game.py
+++ game.py
@@ -6,9 +6,9 @@ import pygame.freetype
 import math
 
 #initial settings
-#fps set in the game loop
 pygame.init()
 
+FPS = 30
 display_width = 1000
 display_height = 700
 Display = pygame.display.set_mode( [display_width,display_height] )
@@ -27,6 +27,8 @@ class BigNumber:
             value /= 1000
             thousand_power += 1
 
+        if value == math.floor(value):
+            value = int(value)
         self.value = value
         self.thousand_power = thousand_power
 
@@ -61,10 +63,10 @@ class BigNumber:
         elif other_Number[1] > self[1]:
             self.value = self.value + other_Number[0] * math.pow(1000, (other_Number[1]) - self.thousand_power)
 
-        if self[0] >= 1000:
+        while self[0] >= 1000:
             self.value /= 1000
             self.thousand_power += 1
-        elif self[0] < 1 and self.thousand_power:
+        while self[0] < 1 and self.thousand_power:
             self.value *= 1000
             self.thousand_power -= 1
 
@@ -81,7 +83,7 @@ class BigNumber:
             self.thousand_power -= 1
         self.value -= other_Number[0]
 
-        while self[0] >= 1000 and self.thousand_power:
+        while self[0] >= 1000:
             self.value /= 1000
             self.thousand_power += 1
 
@@ -123,21 +125,21 @@ class Cookie(pygame.sprite.Sprite):
         if not self.shrunken:
             self.surface = CookieImage
 
-    def check_size(self):
-        return self.shrunken
-
     def shrink(self):
         self.shrunken = 1
-        self.surface = pygame.transform.scale(self.surface, (self.SurfaceWidth * 0.95, self.SurfaceHeight * 0.9))
+        self.surface = pygame.transform.scale(self.surface, (self.SurfaceWidth * 0.9, self.SurfaceHeight * 0.9))
         self.rect = self.surface.get_rect()
+
     def fix_size(self):
         self.shrunken = 0
         self.surface = pygame.transform.scale(self.surface, (self.SurfaceWidth, self.SurfaceHeight))
         self.rect = self.surface.get_rect()
 
-def click():
-    global Money
-    Money.add(MoneyPerClick)
+    def click(self):
+        global Money
+        Money.add(MoneyPerClick)
+
+Current_Page = 1 #used to scroll through upgrade buttons
 
 TopTextFont = pygame.freetype.Font(None, 30)
 SecondTextFont = pygame.freetype.Font(None, 20)
@@ -181,6 +183,10 @@ def draw_default_text(display):
                                                                 (255, 255, 255))
     fourth_rect.center = (int(display_width/2), 675)
     display.blit(fourth_surface, fourth_rect)
+
+    fifth_surface, fifth_rect = TopTextFont.render(f"{Current_Page}", (255, 255, 255))
+    fifth_rect.center = (825, 600)
+    display.blit(fifth_surface, fifth_rect)
 
 UpgradeButtonFont = pygame.freetype.Font(None, 20)
 UpgradeButtonSecondaryFont = pygame.freetype.Font(None, 15)
@@ -272,7 +278,7 @@ class UpgradeButton(pygame.sprite.Sprite):
     def shrink(self):
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             self.shrunken = 1
-            self.surface = pygame.transform.scale(self.surface, (self.SurfaceWidth * 0.95, self.SurfaceHeight * 0.9))
+            self.surface = pygame.transform.scale(self.surface, (self.SurfaceWidth * 0.97, self.SurfaceHeight * 0.9))
             self.rect = self.surface.get_rect()
 
     def fix_size(self):
@@ -286,24 +292,89 @@ class Page:
         self.buttons = args
 
     def update(self):
-        for i in self.buttons:
-            i.update()
+        for j in self.buttons:
+            j.update()
 
     def buy(self):
-        for i in self.buttons:
-            i.buy()
+        for j in self.buttons:
+            j.buy()
 
     def draw(self, surface):
-        for i in self.buttons:
-            i.draw(surface)
+        for j in self.buttons:
+            j.draw(surface)
 
     def shrink(self):
-        for i in self.buttons:
-            i.shrink()
+        for j in self.buttons:
+            j.shrink()
 
     def fix_size(self):
-        for i in self.buttons:
-            i.fix_size()
+        for j in self.buttons:
+            j.fix_size()
+
+
+class OtherButton(pygame.sprite.Sprite):
+    shrunken = 0
+    ButtonWidth = 0
+    ButtonHeight = 0
+    def __init__(self, size:tuple[int, int], pos:tuple[int, int], image_name:str=None, text:str=None, text_size:int=20,
+                 user_event:pygame.event.Event=None):
+        super().__init__()
+        self.size = size
+        self.pos = pos
+        self.image_name = image_name
+        self.text = text
+        self.font = pygame.freetype.Font(None, text_size)
+
+        self.saved_surface = pygame.Surface(size)
+        self.rect = self.saved_surface.fill((180, 180, 180))
+        self.ButtonWidth, self.ButtonHeight = size
+
+        if image_name:
+            self.image = pygame.transform.scale(pygame.image.load(image_name), self.size)
+            self.saved_surface.blit(self.image, self.image.get_rect())
+        elif text:
+            self.text_surface, self.text_rect = self.font.render(text, (255, 255, 255))
+            self.text_rect.center = self.rect.center
+            self.saved_surface.blit(self.text_surface, self.text_rect)
+
+        if user_event:
+            self.user_event = user_event
+
+        self.surface = self.saved_surface.copy()
+
+    def draw(self, surface):
+        if not self.shrunken and self.text:
+            self.saved_surface = pygame.Surface(self.size)
+            self.rect = self.saved_surface.fill((180, 180, 180))
+            self.text_surface, self.text_rect = self.font.render(self.text, (255, 255, 255))
+            self.text_rect.center = self.rect.center
+            self.saved_surface.blit(self.text_surface, self.text_rect)
+            self.surface = self.saved_surface.copy()
+
+        self.rect.center = self.pos
+        surface.blit(self.surface, self.rect)
+
+        if not self.shrunken:
+            self.surface = self.saved_surface.copy()
+
+    def press(self):
+        if self.shrunken and self.rect.collidepoint(pygame.mouse.get_pos()):
+            pygame.event.post(self.user_event)
+
+    def shrink(self):
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            self.shrunken = 1
+            self.surface = pygame.transform.scale(self.saved_surface, (self.ButtonWidth * 0.9, self.ButtonHeight * 0.9))
+            self.rect = self.surface.get_rect()
+            self.rect.center = self.pos
+
+    def fix_size(self):
+        if self.shrunken:
+            self.shrunken = 0
+            self.surface = pygame.transform.scale(self.saved_surface, (self.ButtonWidth, self.ButtonHeight))
+            self.rect = self.surface.get_rect()
+            self.rect.center = self.pos
+
 
 #game loop
 Clock = pygame.time.Clock()
@@ -316,14 +387,37 @@ MainCookie = Cookie()
 Clicker = UpgradeButton("cursor.webp", 0, "Clicker", 1, 25)
 Oven = UpgradeButton("oven.png", 1, "Oven", 5, 150)
 Bakery = UpgradeButton("bakery.png", 2, "Bakery", 25, 1500)
-Factory = UpgradeButton("factory.png", 3, "Factory", 100, 10000)
-Shipyard = UpgradeButton("shipyard.webp", 4, "Shipyard", 300, 99000)
+Factory = UpgradeButton("factory.png", 3, "Factory", 100, 10, cost2=1)
+Shipyard = UpgradeButton("shipyard.webp", 4, "Shipyard", 300, 100, cost2=1)
 Page1 = Page(Clicker, Oven, Bakery, Factory, Shipyard)
+
+Page2PlaceHold = UpgradeButton("Cookie.webp", 5, "Place Holder 1", 10, 0, cps2=1)
+Page2PlaceHold2 = UpgradeButton("Cookie.webp", 6, "Place Holder 2", 10, 0, cps2=2)
+Page2 = Page(Page2PlaceHold, Page2PlaceHold2)
+
+Page3PlaceHold = UpgradeButton("Cookie.webp", 10, "Place Holder 3", 0, 1, cost2=2)
+Page3PlaceHold2 = UpgradeButton("Cookie.webp", 11, "Place Holder 4", 0, 1, cost2=3)
+Page3PlaceHold3= UpgradeButton("Cookie.webp", 12, "Place Holder 5", 0, 1, cost2=4)
+Page3PlaceHold4= UpgradeButton("Cookie.webp", 13, "Place Holder 6", 0, 1, cost2=5)
+Page3PlaceHold5= UpgradeButton("Cookie.webp", 14, "Place Holder 7", 0, 1, cost2=6)
+Page3 = Page(Page3PlaceHold, Page3PlaceHold2, Page3PlaceHold3, Page3PlaceHold4, Page3PlaceHold5)
+
+MaxPages = 3
+PageDown_event = pygame.event.Event(USEREVENT + 0)
+PageDownButton = OtherButton((40, 40), (775, 600), text="<", text_size=30, user_event=PageDown_event)
+PageUp_event = pygame.event.Event(USEREVENT + 1)
+PageUpButton = OtherButton((40, 40), (875, 600), text=">", text_size=30, user_event=PageUp_event)
+
+UncapFPS_event = pygame.event.Event(USEREVENT + 2)
+UncapFPS = OtherButton((140, 40), (int(display_width/2), 600), text="Uncap FPS" if FPS == 30 else "Cap FPS", text_size=20,
+                       user_event=UncapFPS_event)
 
 # MainCookie not included in arrays
 SizeChangesWhenClicked =   [Page1]
 NeedsUpdating =            [Page1]
 NeedsDrawing = [MainCookie, Page1]
+
+OtherButtons = [PageDownButton, PageUpButton, UncapFPS]
 while 1:
 
     for i in NeedsUpdating:
@@ -341,25 +435,68 @@ while 1:
 
             for i in SizeChangesWhenClicked:
                 i.shrink()
+            for i in OtherButtons:
+                i.shrink()
 
         if event.type == MOUSEBUTTONUP:
             if MainCookie.shrunken:
                 MainCookie.fix_size()
-                click()
+                MainCookie.click()
 
             for i in SizeChangesWhenClicked:
                 i.buy()
                 i.fix_size()
+            for i in OtherButtons:
+                i.press()
+                i.fix_size()
+
+    if pygame.event.get(PageDown_event.type):
+        Current_Page = MaxPages if Current_Page == 1 else Current_Page - 1
+
+        if Current_Page == 1:
+            SizeChangesWhenClicked = [Page1]
+            NeedsUpdating = [Page1]
+            NeedsDrawing = [MainCookie, Page1]
+        elif Current_Page == 2:
+            SizeChangesWhenClicked = [Page2]
+            NeedsUpdating = [Page2]
+            NeedsDrawing = [MainCookie, Page2]
+        elif Current_Page == 3:
+            SizeChangesWhenClicked = [Page3]
+            NeedsUpdating = [Page3]
+            NeedsDrawing = [MainCookie, Page3]
+
+    elif pygame.event.get(PageUp_event.type):
+        Current_Page = 1 if Current_Page == MaxPages else Current_Page + 1
+
+        if Current_Page == 1:
+            SizeChangesWhenClicked = [Page1]
+            NeedsUpdating = [Page1]
+            NeedsDrawing = [MainCookie, Page1]
+        elif Current_Page == 2:
+            SizeChangesWhenClicked = [Page2]
+            NeedsUpdating = [Page2]
+            NeedsDrawing = [MainCookie, Page2]
+        elif Current_Page == 3:
+            SizeChangesWhenClicked = [Page3]
+            NeedsUpdating = [Page3]
+            NeedsDrawing = [MainCookie, Page3]
+
+    elif pygame.event.get(UncapFPS_event.type):
+        UncapFPS.text = "Uncap FPS" if UncapFPS.text == "Cap FPS" else "Cap FPS"
+        FPS = 30 if FPS == 0 else 0
 
     Display.fill((0, 0, 0))
 
     #add money since last tick
     if Cookies_per_second.value:
-        Money.add(Cookies_per_second.__copy__().multiply( (Clock.tick(30) / 1000) ))
-    else:
-        Clock.tick(30) #run game at 30 fps
+        Money.add(Cookies_per_second.__copy__().multiply( (Clock.tick(FPS) / 1000) ))
+    else: #needed because cps can be 0, which somehow messes with Clock.get_fps()
+        Clock.tick(FPS)
 
     for i in NeedsDrawing:
+        i.draw(Display)
+    for i in OtherButtons:
         i.draw(Display)
 
     draw_default_text(Display)
